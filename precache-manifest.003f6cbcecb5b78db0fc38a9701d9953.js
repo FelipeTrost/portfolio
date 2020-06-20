4bf44710dd4f5b82288738060306ead6d01a6c67
Felipe Trost <felipe.trost@gmail.com>
2020-06-19 21:06:36 -0400

    Updates

---
+++ precache-manifest.003f6cbcecb5b78db0fc38a9701d9953.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcedd3ff0b216324ec59b0dde053a00c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80fec6f822aacf9e13c2",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "34eb6717c51a517439de",
+    "url": "/portfolio/static/css/main.ca906d68.chunk.css"
+  },
+  {
+    "revision": "80fec6f822aacf9e13c2",
+    "url": "/portfolio/static/js/2.13e58b0a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.13e58b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34eb6717c51a517439de",
+    "url": "/portfolio/static/js/main.4843f2c0.chunk.js"
+  },
+  {
+    "revision": "ea6bde25bed32c94c19a",
+    "url": "/portfolio/static/js/runtime-main.3fdb0160.js"
+  },
+  {
+    "revision": "6c65474488166be9d941a7c5a4006ea4",
+    "url": "/portfolio/static/media/bg.6c654744.jpg"
+  }
+]);
